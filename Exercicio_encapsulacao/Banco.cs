d524eff1c9d0f79700c71f15cde4cf66ad6debb7
Mystery547 <56850984+Mystery547@users.noreply.github.com>
2020-05-08 15:40:24 -0300

    Add files via upload

---
+++ Exercicio_encapsulacao/Banco.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace Exercicio_encapsulacao
+{
+    class Banco
+    {
+        public int NumeroDaConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public Banco()
+        {
+        }
+
+        public Banco(int numeroDaConta, string nome)
+        {
+            NumeroDaConta = numeroDaConta;
+            Nome = nome;
+        }
+
+        public Banco(int numeroDaConta, string nome, double saldo) : this(numeroDaConta, nome)
+        {
+            Deposito(saldo);
+        }
+
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo -= saque + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroDaConta
+                + ", Titular: "
+                + Nome
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio_encapsulacao/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_encapsulacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco b = new Banco();
+
+            Console.Write("Entre com o número da conta: ");
+            int numeroDaConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o Titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char confirmacao = char.Parse(Console.ReadLine());
+
+            while(confirmacao != 's' && confirmacao != 'S' && confirmacao != 'n' && confirmacao != 'N')
+            {
+                Console.WriteLine();
+                Console.WriteLine("Favor fornecer resposta válida!");
+                Console.WriteLine();
+
+                Console.Write("Haverá depósito inicial (s/n)? ");
+                confirmacao = char.Parse(Console.ReadLine());
+
+                if (confirmacao == 's' || confirmacao == 'S')
+                {
+                    Console.Write("Entre o valor de depósito inicial: ");
+                    double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    b = new Banco(numeroDaConta, nome, saldo);
+                }
+                else
+                {
+                    b = new Banco(numeroDaConta, nome);
+                }
+            }          
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(b);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b.Deposito(deposito);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(b);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b.Saque(saque);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(b);
+        }
+    }
+}
